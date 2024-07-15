b7c62e4503baca1a223e97a132368732a61daa04
Avishek <avishekmishra56@gmail.com>
2024-07-15 15:49:01 +0530

    "Remove Payment component and its related code from Marketing component"

--- src/components/Marketing.jsx
+++ src/components/Marketing.jsx
@@ -1,26 +1,15 @@
 "use client"
 
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import './Marketing.css';
 import UserResume from './UserResume';
 import { UserDataContext } from '@/context/UserDataContext';
-import Payment from './Payment';
 
 function Marketing() {
   const { uploadStatus } = useContext(UserDataContext);
   console.log(`running marketing.jsx when uploaded is ${uploadStatus.uploaded}`);
-  const [payment, setPayment] = useState(false)
 
-  const handlePayment = () => {
-    setPayment(true)
-  }
-  if (payment) {
-    return (
-      <div>
-        <Payment />
-      </div>
-    )
-  }else if (uploadStatus.uploaded) {
+  if (uploadStatus.uploaded) {
     console.log('Rendering UserResume component');
     return (
       <div className='w-[50%] h-[100%] max1078:w-[90vw]'>
@@ -37,9 +26,6 @@ function Marketing() {
             <p className="text-[1.8rem] text-[#c7c7c7] leading-relaxed max600:text-[5vw] max600:text-center">
               Create a standout resume that gets noticed by employers. Our easy-to-use builder guides you through the process, ensuring you highlight your skills and experience effectively.
             </p>
-            <button className='border border-white text-white' onClick={handlePayment}>
-              Payment
-            </button>
           </div>
         </section>
       </div>
