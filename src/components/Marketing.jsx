04688f39bf4f926843c489fe0855dc2bff2b68c9
Avishek <avishekmishra56@gmail.com>
2024-07-18 07:05:46 +0530

    "Added fade-in animations to Marketing component"

--- src/components/Marketing.jsx
+++ src/components/Marketing.jsx
@@ -15,18 +15,20 @@ function Marketing() {
   if (uploadStatus.uploaded) {
     console.log('Rendering UserResume component');
     return (
-      <div className='w-[50%] h-[100%] max1078:w-[90vw]'>
+      <div className='w-[50%] h-[100%] max1078:w-[90vw] fade-in-up'>
         <UserResume fileUrl={uploadStatus.fileUrl} />
       </div>
     );
   } else {
     return (
       <div className='w-[60%] max1078:w-[90vw]'>
-        <section className='py-12 mt-[10rem] max1078:mt-0'>
+        <section className='py-12 mt-[10rem] max1078:mt-0 fade-in-up'>
           <div className="container mx-auto px-4">
-            <h2 className="text-[4.5vw] font-semibold text-gray-800 mb-4 rainbow-text-animation max767:text-[10vw] max767:text-center">Build Your Perfect Resume</h2>
-            <p className="text-[1.5vw] text-[#c7c7c7] text-justify leading-relaxed max600:text-[5vw] max600:text-center source">
-            Craft an Impressive Resume that Captures Employers' Attention. Our easy-to-use builder guides you through the process, ensuring you highlight your skills and experience effectively. Upload your resume for free review.
+            <h2 className="text-[4.5vw] font-semibold text-gray-800 mb-4 rainbow-text-animation max767:text-[10vw] max767:text-center fade-in">
+              Build Your Perfect Resume
+            </h2>
+            <p className="text-[1.5vw] text-[#c7c7c7] text-justify leading-relaxed max600:text-[5vw] max600:text-center source fade-in">
+              Craft an Impressive Resume that Captures Employers' Attention. Our easy-to-use builder guides you through the process, ensuring you highlight your skills and experience effectively. Upload your resume for free review.
             </p>
           </div>
         </section>
